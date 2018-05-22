7f93b8df051ea6e54b65d450e91a65a2917bd6f9
purnamaekawijaya <37338034+purnamaekawijaya@users.noreply.github.com>
2018-05-23 02:54:18 +0700

    Add files via upload

---
+++ belajarngodingV1.1.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("IDEALIS");
+  delay(500);
+  lcd.clear();
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("UKUR BERAT IDEAL");
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+}
